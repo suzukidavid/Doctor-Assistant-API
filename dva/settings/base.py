ec764ef9f79f64595f6d55c9b976a6784d7c3703
suzukidavid <superdevk@gmail.com>
2021-06-28 18:22:32 +0600

    Update Templates dir

--- dva/settings/base.py
+++ dva/settings/base.py
@@ -74,7 +74,7 @@ ROOT_URLCONF = 'dva.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(PROJECT_DIR, 'templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
